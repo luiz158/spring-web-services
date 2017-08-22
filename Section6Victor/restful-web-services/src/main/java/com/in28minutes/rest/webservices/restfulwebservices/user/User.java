2bbce4f3a929af6d4111776dddd2680afb88ac31
vherasme <victor.herasme@sogeti.com>
2017-08-22 13:52:35 +0200

    HATEOAS and Validation

--- Section6Victor/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ Section6Victor/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,12 +2,17 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
 
+	@Size(min = 2, message = "User should have two or more characters")
 	private String name;
 
+	@Past
 	private Date birthDate;
 
 	public User(int id, String name, Date birthDate) {
--- Section6Victor/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ Section6Victor/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,7 +3,11 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 @RestController
 public class UserResource {
@@ -29,17 +35,23 @@ public class UserResource {
 	// GET users/{id}
 	// retrieve single user
 	@GetMapping("/users/{id}")
-	public User getUser(@PathVariable Integer id) {
+	public Resource<User> getUser(@PathVariable Integer id) {
 		User user = service.findOne(id);
 
 		if (user == null)
 			throw new UserNotFoundException("User with id " + id + " does not exist");
 
-		return user;
+		Resource<User> resource = new Resource<>(user);
+
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+		resource.add(linkTo.withRel("all-users"));
+
+		return resource;
 	}
 
 	@PostMapping("/users")
-	public ResponseEntity<?> createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);
 
 		URI locacion = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
